1c2404139b3d4c8538d51c75eaf76f76ccdc9eec
Sue Duclos <sue.duclos@gmail.com>
2020-02-03 18:09:22 -0800

    attempted question 1

--- code-challenges/chaining-methods/challenges-10.test.js
+++ code-challenges/chaining-methods/challenges-10.test.js
@@ -13,7 +13,7 @@ For example, count(5, [[1, 3, 5, 7, 9], [5, 5, 5], [1, 2, 3]]) returns 4.
 
 const count = (target, input) => {
   // Solution code here...
-};
+  return count.filter(target);
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
