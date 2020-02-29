661ccbfea5e90a66869d13dc75450c6c636406eb
Sue Duclos <sue.duclos@gmail.com>
2020-02-29 04:51:00 -0800

    completed question 1 and 2

--- code-challenges/chaining-methods/challenges-10.test.js
+++ code-challenges/chaining-methods/challenges-10.test.js
@@ -13,7 +13,16 @@ For example, count(5, [[1, 3, 5, 7, 9], [5, 5, 5], [1, 2, 3]]) returns 4.
 
 const count = (target, input) => {
   // Solution code here...
-  return count.filter(target);
+  let count = 0;
+  input.forEach(arr => {
+    arr.forEach(value => {
+      if (target === value) {
+        count++;
+      }
+    })
+  })
+  return count;
+};
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
@@ -27,6 +36,13 @@ For example, [[1, 2, 3, 4, 5], [6, 7, 2, 4, 5, 7], [9, 2, 3, 6,]] returns 66.
 
 const totalSum = (input) => {
   // Solution code here...
+  let arrSum = 0;
+  for (let i =0; i < input.length; i++) {
+    for (let j = 0; j <input[i].length; j++) {
+      arrSum += input[i][j];
+    }
+  }
+  return arrSum;
 };
 
 /* ------------------------------------------------------------------------------------------------
