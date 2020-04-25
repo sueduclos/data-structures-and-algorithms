b8ca71903257f073d50441d3090e49c23729d8ad
Sue Duclos <sue.duclos@gmail.com>
2020-04-25 08:53:25 -0700

    started challenge

---
+++ challenges/fifoAnimalShelter/fifo-animal-shelter.js
@@ -0,0 +1,23 @@
+'use strict';
+
+class Node {
+  constructor(val) {
+    this.animal = val;
+    this.next = null;
+  }
+}
+
+class AnimalShelter {
+  constructor () {
+    this.front = null;
+    this.rear = null;
+  }
+
+  enqueue(animal) {
+
+  }
+
+  dequeue(pref) {
+
+  }
+}
