9d42ff352abde0a353ddd5cfa506f2cde481d45e
Sue Duclos <sue.duclos@gmail.com>
2020-04-28 19:12:22 -0700

    added enqueue

--- challenges/fifoAnimalShelter/fifo-animal-shelter.js
+++ challenges/fifoAnimalShelter/fifo-animal-shelter.js
@@ -14,10 +14,35 @@ class AnimalShelter {
   }
 
   enqueue(animal) {
+    let newNode = new Node(animal);
 
+    if (!this.front && !this.rear) {
+        this.front = newNode;
+        this.rear - newNode;
+        return;
+    }
+
+    this.rear.next = newNode;
+    this.rear = newNode;
   }
 
   dequeue(pref) {
+    if (!(pref ==='dog' || pref === `cat`)) return null;
+
+    let currentNode = this.front;
+    let prevNode = null;
+    let foundAnimal = null;
+
+    while(currentNode) {
+      let animal = currentNode.val;
+
+      if(animal.species === pref)
+        if (!prevNode) {
+          this.front = currentNode.next;
+          currentNode.next = null;
+          return currentNode;
+        }
+    }
 
   }
 }
