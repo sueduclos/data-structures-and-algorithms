8d42310ae021ba6fd2b0e248e783df29ba03f1ea
Sue Duclos <sue.duclos@gmail.com>
2020-04-28 21:05:05 -0700

    created tests

---
+++ challenges/multiBracketValidation/multi-bracket-validation.js
@@ -0,0 +1,22 @@
+// 'use strict';
+
+// import { tsConstructorType } from "@babel/types";
+
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
+// class Stack {
+//   constructor() {
+//     this.top = null;
+//   }
+
+//   push(val) {
+//     let newNode = new Node(val);
+//   }
+// }
+
+// }
