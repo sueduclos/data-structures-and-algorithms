d6700cd5a923dbe41d54444b9e2e4cce73062cb9
Sue Duclos <sue.duclos@gmail.com>
2020-06-27 08:47:19 -0700

    updated challenge

---
+++ code-challenges/repeatedWord/repeated-word.js
@@ -0,0 +1,11 @@
+'use strict';
+
+//Write a function that accepts a lengthy string parameter.
+//Find the first repeated word in a book.
+
+function repeatedWord(str) {
+  if (typeof str !== 'string') return null;
+
+}
+
+module.exports = repeatedWord;
