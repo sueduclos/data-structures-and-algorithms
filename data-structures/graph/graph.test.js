5abde540bfbb74deaad3eb22555ebf17f197fc22
Sue Duclos <sue.duclos@gmail.com>
2020-07-14 18:04:34 -0700

    file setup

---
+++ data-structures/graph/graph.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Node can be successfully added to the graph
+// An edge can be successfully added to the graph
+// A collection of all nodes can be properly retrieved from the graph
+// All appropriate neighbors can be retrieved from the graph
+// Neighbors are returned with the weight between nodes included
+// The proper size is returned, representing the number of nodes in the graph
+// A graph with only one node and edge can be properly returned
+// An empty graph properly returns null
+
